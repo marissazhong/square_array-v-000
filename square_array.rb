08a9c2eebbc603db2fb4f57981d28c7697e472ce
Marissa Zhong <mwz2101@columbia.edu>
2018-06-25 21:09:23 +0000

    Done.

--- square_array.rb
+++ square_array.rb
@@ -1,3 +1,12 @@
+
 def square_array(array)
-  # your code here
+  new_array = []
+  array.each do |num|
+    new_array.push(num**2)
+  end
+  return new_array
 end
+
+#def square_array(array)
+#  new_array = array.collect{|x| x**2}
+#end
