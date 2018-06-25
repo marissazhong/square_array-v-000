08fb6164a833f3b260ee91ffa123df91b6517272
Marissa Zhong <mwz2101@columbia.edu>
2018-06-25 21:09:08 +0000

    automatically backed up by learn

--- square_array.rb
+++ square_array.rb
@@ -1,12 +1,12 @@
 
-#def square_array(array)
-#  new_array = []
-#  array.each do |num|
-#    new_array.push(num**2)
-#  end
-#  return new_array
-#end
-
 def square_array(array)
-  new_array = array.collect{|x| x**2}
+  new_array = []
+  array.each do |num|
+    new_array.push(num**2)
+  end
+  return new_array
 end
+
+#def square_array(array)
+#  new_array = array.collect{|x| x**2}
+#end
